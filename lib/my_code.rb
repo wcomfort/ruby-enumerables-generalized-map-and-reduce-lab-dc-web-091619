f6d3c9364f87a03842f3f3f40bf29075f4523b0c
Will Comfort <comfortwb21@gmail.com>
2019-09-05 14:48:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map(array)
+  new = []
+  i = 0 
+    while i < array.length do 
+    new.push(yield(array[i]))
+     i += 1 
+  end 
+  return new 
+end
+
+def reduce(array, staring_value = 0)
+    if staring_value
+      num1 = staring_value
+      i = 0 
+    else 
+      num1 = array[0]
+      i = 1 
+      yield(array[i] += staring_value)
+    end 
+  while i < array.length do
+    yield(num1, array[i])
+  end 
+  i += 1 
+end 
