ea7c420d50f7d55926dc74ab40d90baf577c550e
Will Comfort <comfortwb21@gmail.com>
2019-09-05 14:59:46 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map(array)
+  new = []
+  i = 0 
+    while i < array.length do 
+    new.push(yield(array[i]))
+     i += 1 
+  end 
+  return new 
+end
+
+def reduce(array, sv = nil)
+    if sv
+      num1 = sv
+      i = 0 
+    else 
+      num1 = array[0]
+      i = 1 
+    end 
+  while i < array.length do
+   num1 = yield(num1, array[i])
+   i += 1 
+  end 
+  num1
+end 
